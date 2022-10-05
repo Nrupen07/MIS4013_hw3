75a5c1447a1de166ad119816b2660773c3e24a9b
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-10-05 05:48:23 -0500

    Update instructor-cards.php

--- instructor-cards.php
+++ instructor-cards.php
@@ -11,9 +11,9 @@
 <div class="card-group">
     <?php
 $servername = "localhost";
-$username = "projecto_homework3";
-$password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$username = "nrupenou_hw3";
+$password = "zh~FG(N@#kdG";
+$dbname = "nrupenou_hw3";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
